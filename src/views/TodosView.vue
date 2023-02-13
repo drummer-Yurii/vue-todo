4e3ce0244201936e9915d98eef3c474cd5c1f9b6
Yurii Rybachok <drummer2009@ukr.net>
2023-02-13 21:15:45 +0200

    add todo item component

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref } from 'vue';
 import { uid } from 'uid';
+import { Icon } from '@iconify/vue';
 import TodoCreator from '../components/TodoCreator.vue';
+import TodoItem from '../components/TodoItem.vue';
 
 const todoList = ref([]);
 const createTodo = (todo) => {
@@ -18,6 +20,13 @@ const createTodo = (todo) => {
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo" />
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem v-for="todo in todoList" :todo="todo" :key="todo" />
+    </ul>
+    <p class="todos-msg" v-else>
+      <Icon icon="noto-v1:sad-but-relieved-face" width="22" />
+      <span>You have no todo's to complete! Add one!</span>
+    </p>
   </main>
 </template>
 
@@ -29,9 +38,26 @@ main {
   width: 100%;
   margin: 0 auto;
   padding: 40px 16px;
+
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
