b9ad81671a7165b1ea55b682e1aca25f33f22a92
Yurii Rybachok <drummer2009@ukr.net>
2023-02-14 11:39:21 +0200

    completed todo state computed

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch } from 'vue';
+import { ref, watch, computed } from 'vue';
 import { uid } from 'uid';
 import { Icon } from '@iconify/vue';
 import TodoCreator from '../components/TodoCreator.vue';
@@ -11,7 +11,11 @@ watch(todoList, () => {
   setTodoListLocalStorage();
 }, {
   deep: true,
-})
+});
+
+const todoCompleted = computed(() => {
+  return todoList.value.every((todo) => todo.isCompleted);
+});
 
 const fetchTodoList = () => {
   const savedTodoList = JSON.parse(localStorage.getItem("todoList"));
@@ -63,6 +67,10 @@ const deleteTodo = (todoId) => {
     <p class="todos-msg" v-else>
       <Icon icon="noto-v1:sad-but-relieved-face" width="22" />
       <span>You have no todo's to complete! Add one!</span>
+    </p>
+    <p v-if="todoCompleted && todoList.length > 0" class="todos-msg">
+      <Icon icon="noto-v1:party-popper" />
+      <span>You have completed all your todos!</span>
     </p>
   </main>
 </template>
